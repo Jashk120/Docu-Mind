154679b12478b8a45687ed4f916ef02d4e2fa1dd
Jayesh Kathale <jayeshkathale24@gmail.com>
2026-05-02 17:52:23 +0530

    Fixed few auth issues

--- frontend/src/auth.ts
+++ frontend/src/auth.ts
@@ -7,7 +7,6 @@ import { prisma } from "@/lib/db";
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   secret: process.env.NEXTAUTH_SECRET,
-  trustHost: true,
   pages: {
     signIn: "/",
     error: "/",
